76b714272677ba806367891635db39beb23a5d81
burakhasici <burakhasici@gmail.com>
2025-11-17 19:23:19 +0300

    Add global styles, reset styles, and sign-in page layout
    ## Changes
    - Introduced global SCSS variables for colors and font sizes.
    - Added a reset stylesheet to standardize element styles across browsers.
    - Created a new sign-in page layout with responsive design using Flexbox.
    - Included a background image for the sign-in page.

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,5 +1,28 @@
+import Image from "next/image";
+import styles from "./page.module.scss";
+
 const Page = () => {
-  return <div>SignInPage</div>;
+  return (
+    <div className={styles.page}>
+      <div className={styles.column}>
+        <p>Sign In</p>
+      </div>
+      <div
+        className={styles.column}
+        style={{ position: "relative", minHeight: "100vh" }}
+      >
+        <Image
+          src="/login-bg.png"
+          alt="login-bg"
+          fill
+          style={{
+            objectFit: "cover",
+          }}
+          sizes="50vw"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Page;
