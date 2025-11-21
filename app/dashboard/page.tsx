49b02807feeddcf7ec570100e57571e096c3cf8e
burakhasici <burakhasici@gmail.com>
2025-11-21 14:18:32 +0300

    refactor: Update dashboard layout and enhance styling
    - Removed background and border styles from the right panel for a cleaner look.
    - Added a title to the main content area and reorganized stat card components for better structure.
    - Updated the right panel to include a new title for improved clarity.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,13 +5,31 @@ import Sidebar from "@/components/dashboard/sidebar";
 const DashboardPage = () => {
   return (
     <div className={styles["dashboard-layout"]}>
-      {/* Sidebar */}
       <Sidebar />
 
       {/* Main Content */}
       <div className={styles["main-content"]}>
         {/* Stats Cards Grid */}
+        <h1>Dashboard</h1>
         <div className={styles["stats-grid"]}>
+          <StatCard
+            label={"Total Balance"}
+            value={"$100,000"}
+            iconSrc={"/icons/wallets.svg"}
+            variant="dark"
+          />
+          <StatCard
+            label={"Total Spending"}
+            value={"$100,000"}
+            iconSrc={"/icons/wallets.svg"}
+            variant="light"
+          />
+          <StatCard
+            label={"Total Spending"}
+            value={"$100,000"}
+            iconSrc={"/icons/wallets.svg"}
+            variant="light"
+          />
           <div>{/* Stat Card 1 */}</div>
           <div>{/* Stat Card 2 */}</div>
           <div>{/* Stat Card 3 */}</div>
@@ -19,27 +37,6 @@ const DashboardPage = () => {
 
         {/* Chart and Table Section */}
         <div className={styles["content-grid"]}>
-          <h1>Dashboard</h1>
-          <div className={styles["stats-grid"]}>
-            <StatCard
-              label={"Total Balance"}
-              value={"$100,000"}
-              iconSrc={"/icons/wallets.svg"}
-              variant="dark"
-            />
-            <StatCard
-              label={"Total Spending"}
-              value={"$100,000"}
-              iconSrc={"/icons/wallets.svg"}
-              variant="light"
-            />
-            <StatCard
-              label={"Total Spending"}
-              value={"$100,000"}
-              iconSrc={"/icons/wallets.svg"}
-              variant="light"
-            />
-          </div>
           <div>{/* Chart */}</div>
           <div>{/* Table */}</div>
         </div>
@@ -47,7 +44,8 @@ const DashboardPage = () => {
 
       {/* Right Panel */}
       <div className={styles["right-panel"]}>
-        {/* Scheduled Transfers List */}
+        <h1>Wallets</h1>
+
         <div className={styles["transfers-list"]}>
           <div className={styles["transfer-item"]}>
             <div>{/* Icon */}</div>
